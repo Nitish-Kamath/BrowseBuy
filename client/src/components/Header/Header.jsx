b4ecdeb9a7dd47a962d687ca920a02d87f01585a
Nitish-Kamath <nitishkamath63590@gmail.com>
2024-07-27 20:03:42 +0530

    Header Update

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,25 +1,17 @@
-import "./Header.scss";
-
 import { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { TbSearch } from "react-icons/tb";
 import { CgShoppingCart } from "react-icons/cg";
 import { AiOutlineHeart } from "react-icons/ai";
-
-import Search from "./Search/Search";
-import Cart from "../Cart/Cart";
-import { Context } from "../../utils/context";
-
 import "./Header.scss";
+import Search from "./Search/Search";
+import { Context } from "../../utils/context";
+import Cart from "../Cart/Cart";
+
 const Header = () => {
-  // useEffect bade kaam ki cheez hai as soon as page is loaded
-  // useEffect is called
-  // on each stateChange useEffect is called and we can write our logic there
-
   const [scrolled, setScrolled] = useState(false);
-  const [showCart, setShowCart] = useState(false);
-  const [showSearch, setShowSearch] = useState(false);
-
+  const [searchModal, setSearchModal] = useState(false);
+  const navigate = useNavigate();
   const handleScroll = () => {
     const offset = window.scrollY;
     if (offset > 200) {
@@ -32,31 +24,33 @@ const Header = () => {
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
   }, []);
+
+  const { cartCount, showCart, setShowCart } = useContext(Context);
+
   return (
-    /* // basically here we may have two class depending upon scrolled state //
-      if scrolled is true then class is main_header and sticky_header // if
-      scrolled is false then classis only main_header */
     <>
-      <header className={`main_header ${scrolled ? "sticky_header" : ""}`}>
-        <div className="header_content">
+      <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
+        <div className="header-content">
           <ul className="left">
-            <li>Home</li>
-            <li>About</li>
+            <li onClick={() => navigate("/")}>Home</li>
+            <li onClick={() => navigate("/about")}>About</li>
             <li>Categories</li>
           </ul>
-          <div className="Center">BrowseBuy.</div>
+          <div className="center" onClick={() => navigate("/")}>
+            JSDEVSTORE.
+          </div>
           <div className="right">
-            <TbSearch onClick={() => setShowSearch(true)} />
+            <TbSearch onClick={() => setSearchModal(true)} />
             <AiOutlineHeart />
-            <span className="cart_icon" onClick={() => setShowCart(true)}>
+            <span className="cart-icon" onClick={() => setShowCart(true)}>
               <CgShoppingCart />
-              <span>5</span>
+              {!!cartCount && <span>{cartCount}</span>}
             </span>
           </div>
         </div>
       </header>
-      {showCart && <Cart setShowCart={setShowCart} />}
-      {showSearch && <Search setShowSearch={setShowSearch} />}
+      {searchModal && <Search setSearchModal={setSearchModal} />}
+      {showCart && <Cart />}
     </>
   );
 };
