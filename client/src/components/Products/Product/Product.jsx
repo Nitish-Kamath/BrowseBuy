a1555f96d03c65a0d1c1f9143ecdd572d22cc3de
Nitish-Kamath <nitishkamath63590@gmail.com>
2024-07-27 20:10:15 +0530

    Products update

--- client/src/components/Products/Product/Product.jsx
+++ client/src/components/Products/Product/Product.jsx
@@ -1,17 +1,20 @@
-import "./Product.scss";
+import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./Product.scss";
 
-const Product = ({ id, data }) => {
-  const naviagate = useNavigate();
+const Product = ({ data, id }) => {
+  const navigate = useNavigate();
   return (
-    <div className="product_card" onClick={() => naviagate("/product/" + id)}>
+    <div className="product-card" onClick={() => navigate("/product/" + id)}>
       <div className="thumbnail">
         <img
-          src={process.env.REACT_APP_DEV_URL + data.img.data[0].attributes.url}
-          alt="product Image"
+          src={
+            process.env.REACT_APP_STRIPE_APP_DEV_URL +
+            data.image.data[0].attributes.url
+          }
         />
       </div>
-      <div className="prod_details">
+      <div className="prod-details">
         <span className="name">{data.title}</span>
         <span className="price">&#8377;{data.price}</span>
       </div>
--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,12 +1,11 @@
 import "./Products.scss";
-
 import Product from "./Product/Product";
+
 const Products = ({ products, innerPage, headingText }) => {
-  // if (!products) return;
   return (
-    <div className="products_container">
-      {!innerPage && <div className="sec_heading">{headingText} </div>}
-      <div className="products">
+    <div className="products-container">
+      {!innerPage && <div className="sec-heading">{headingText}</div>}
+      <div className={`products ${innerPage ? "innerPage" : ""}`}>
         {products?.data?.map((item) => (
           <Product key={item.id} id={item.id} data={item.attributes} />
         ))}
