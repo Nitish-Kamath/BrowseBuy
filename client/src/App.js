613e208ae9bad84cb6284cbd44b600008c45a112
Nitish-Kamath <nitishkamath63590@gmail.com>
2024-07-27 22:39:42 +0530

    App and Index update

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
 
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
@@ -7,12 +8,12 @@ import Category from "./components/Category/Category";
 import SingleProduct from "./components/SingleProduct/SingleProduct";
 import Newsletter from "./components/Footer/Newsletter/Newsletter";
 import AppContext from "./utils/context";
+
 function App() {
   return (
     <BrowserRouter>
       <AppContext>
         <Header />
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/category/:id" element={<Category />} />
