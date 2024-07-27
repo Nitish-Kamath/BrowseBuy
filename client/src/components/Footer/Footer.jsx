1ea896878304d4ad75b5a88a827c513315dd41fc
Nitish-Kamath <nitishkamath63590@gmail.com>
2024-07-27 20:01:05 +0530

    Footer Update

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,51 +1,46 @@
-import "./Footer.scss";
-
 import React from "react";
+import "./Footer.scss";
 import { FaLocationArrow, FaMobileAlt, FaEnvelope } from "react-icons/fa";
-
 import Payment from "../../assets/payments.png";
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer_content">
+    <div className="footer">
+      <div className="footer-content">
         <div className="col">
           <div className="title">About</div>
           <div className="text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-            voluptatem est placeat vero? Debitis sunt magni praesentium deleniti
-            illo vero doloribus exercitationem ea repellendus reprehenderit
-            aspernatur laborum, sapiente maxime id!
+            Voluptatem accusantium doloremque laudantium, totam rem aperiam,
+            eaque ipsa quae ab illo inventore veritatis et quasi architecto
+            beatae vitae dicta sunt explicabo eaque ipsa quae ab illo.
           </div>
         </div>
-
         <div className="col">
           <div className="title">Contact</div>
-          <div className="c_item">
+          <div className="c-item">
             <FaLocationArrow />
             <div className="text">
-              venu Vihar Colony, Patel Nagar, Patna 800023
+              Kayaloram Rd, Punnamada, Kottankulangara, Alappuzha, Kerala,
+              688006
             </div>
           </div>
-          <div className="c_item">
+          <div className="c-item">
             <FaMobileAlt />
-            <div className="text">Phone: 8789102621</div>
+            <div className="text">Phone: 0471 272 0261</div>
           </div>
-          <div className="c_item">
+          <div className="c-item">
             <FaEnvelope />
-            <div className="text">Email: nitish123@gmail.com</div>
+            <div className="text">Email: store@jsdev.com</div>
           </div>
         </div>
-
         <div className="col">
           <div className="title">Categories</div>
-          <span className="text">HeadPhones</span>
+          <span className="text">Headphones</span>
           <span className="text">Smart Watches</span>
           <span className="text">Bluetooth Speakers</span>
           <span className="text">Wireless Earbuds</span>
           <span className="text">Home Theatre</span>
           <span className="text">Projectors</span>
         </div>
-
         <div className="col">
           <div className="title">Pages</div>
           <span className="text">Home</span>
@@ -56,17 +51,15 @@ const Footer = () => {
           <span className="text">Contact Us</span>
         </div>
       </div>
-
-      <div className="bottom_bar">
-        <div className="bottom_bar_content">
-          <div className="text">
-            BrowseBuy 2024 CREATED BY Nitish. PREMIUM E-COMMERCE SOLUTIONS.
-          </div>
-
+      <div className="bottom-bar">
+        <div className="bottom-bar-content">
+          <span className="text">
+            JSDEVSTORE 2022 CREATED BY JS DEV. PREMIUM E-COMMERCE SOLUTIONS.
+          </span>
           <img src={Payment} />
         </div>
       </div>
-    </footer>
+    </div>
   );
 };
 
--- client/src/components/Footer/Newsletter/Newsletter.jsx
+++ client/src/components/Footer/Newsletter/Newsletter.jsx
@@ -1,5 +1,3 @@
-import "./Newsletter.scss";
-
 import React from "react";
 import {
   FaFacebookF,
@@ -7,37 +5,34 @@ import {
   FaInstagram,
   FaLinkedinIn,
 } from "react-icons/fa";
-
+import "./Newsletter.scss";
 const Newsletter = () => {
   return (
-    <div className="newsletter_section">
-      <div className="newsletter_content">
-        <span className="small_text">Newsletter</span>
-        <span className="big_text">Sign Up for Latest Updates and Offers</span>
+    <div className="newsletter-section">
+      <div className="newsletter-content">
+        <span className="small-text">Newsletter</span>
+        <span className="big-text">Sign up for latest updates and offers</span>
         <div className="form">
           <input type="text" placeholder="Email Address" />
           <button>Subscribe</button>
         </div>
-        <div className="text">
+        <span className="text">
           Will be used in accordance with our Privacy Policy
-        </div>
-        <div className="social_icons">
-          <div className="icon">
-            <FaFacebookF size={14} />
-          </div>
-
-          <div className="icon">
-            <FaTwitter size={14} />
-          </div>
-
-          <div className="icon">
-            <FaInstagram size={14} />
-          </div>
-
+        </span>
+        <span className="social-icons">
           <div className="icon">
             <FaLinkedinIn size={14} />
           </div>
-        </div>
+          <div className="icon">
+            <FaFacebookF size={14} />
+          </div>
+          <div className="icon">
+            <FaTwitter size={14} />
+          </div>
+          <div className="icon">
+            <FaInstagram size={14} />
+          </div>
+        </span>
       </div>
     </div>
   );
